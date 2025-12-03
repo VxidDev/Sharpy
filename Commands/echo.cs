477dd0032da7c4647b8548d829848b9ec2fad7cd
Void <stas050595@gmail.com>
2025-12-03 17:04:15 +0000

    Modify Echo.Run to handle variables and logging
    Enhanced the Run method to support variable logging and error handling.

--- Commands/echo.cs
+++ Commands/echo.cs
@@ -1,10 +1,25 @@
 using System;
+using System.Diagnostics.SymbolStore;
 namespace Sharpy.Commands;
 static class Echo {
-    public static void Run(string input , Func<string , string[] , bool> CheckIfHelp) {
+    public static void Run(string input , Func<string , string[] , bool> CheckIfHelp , Dictionary<string , string> Vars , Action<string , string> Log) {
         string[] items = input.Split();
 
         if (CheckIfHelp("echo", items)) return;
+
+        bool isVar = false;
+
+        if (items.Contains("--var") || items.Contains("-v")) isVar = true;
+
+        if (isVar) {
+            if (!Vars.ContainsKey(items[1])) {
+                Log("Unknown Variable.", "err");
+                return;
+            } else {
+                Console.WriteLine(Vars[items[1]]);
+                return;
+            }
+        }
 
         Console.WriteLine(input);
     }
